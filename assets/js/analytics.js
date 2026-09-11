2613ddbdb32f26625551224473bcc7b0c33c0102
sargutportfolio <aydinsargut@gmail.com>
2026-09-11 15:33:00 +0300

    refactor: externalize analytics bootstrap for CSP

---
+++ assets/js/analytics.js
@@ -0,0 +1,15 @@
+/* Analytics bootstrap: externalized so CSP does not require unsafe-inline. */
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-H894LKL93J');
+
+/* Yandex.Metrika counter */
+(function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112473942', 'ym');
+
+    ym(112473942, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
