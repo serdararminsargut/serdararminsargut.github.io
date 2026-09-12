a593512fdf57ca6806968c62676649f0ffc397a7
sargutportfolio <aydinsargut@gmail.com>
2026-09-12 23:08:26 +0300

    perf: defer third-party analytics until after initial render

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,15 +1,57 @@
-/* Analytics bootstrap: externalized so CSP does not require unsafe-inline. */
+/* Analytics bootstrap: defer third-party network work until after initial render. */
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-H894LKL93J');
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-H894LKL93J');
 
-/* Yandex.Metrika counter */
-(function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112473942', 'ym');
+(function () {
+  var loaded = false;
+  function loadAnalytics() {
+    if (loaded) return;
+    loaded = true;
 
-    ym(112473942, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+    /* Google Analytics */
+    if (!document.querySelector('script[data-sas-ga]')) {
+      var ga = document.createElement('script');
+      ga.async = true;
+      ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-H894LKL93J';
+      ga.setAttribute('data-sas-ga', '1');
+      document.head.appendChild(ga);
+    }
+
+    /* Yandex.Metrika */
+    window.ym = window.ym || function(){(window.ym.a = window.ym.a || []).push(arguments);};
+    window.ym.l = +new Date();
+    if (!document.querySelector('script[data-sas-ym]')) {
+      var ymScript = document.createElement('script');
+      ymScript.async = true;
+      ymScript.src = 'https://mc.yandex.ru/metrika/tag.js?id=112473942';
+      ymScript.setAttribute('data-sas-ym', '1');
+      document.head.appendChild(ymScript);
+    }
+    window.ym(112473942, 'init', {
+      ssr: true,
+      webvisor: true,
+      clickmap: true,
+      ecommerce: 'dataLayer',
+      referrer: document.referrer,
+      url: location.href,
+      accurateTrackBounce: true,
+      trackLinks: true
+    });
+  }
+
+  function scheduleAnalytics() {
+    if ('requestIdleCallback' in window) {
+      window.requestIdleCallback(loadAnalytics, {timeout: 2500});
+    } else {
+      window.setTimeout(loadAnalytics, 1500);
+    }
+  }
+
+  if (document.readyState === 'complete') {
+    scheduleAnalytics();
+  } else {
+    window.addEventListener('load', scheduleAnalytics, {once: true});
+  }
+})();
